3be21adb6cd5dad8c006ed3a7e8deb346396309b
Kadir Yilmaz <ulas-k.yilmaz@live.fr>
2025-07-14 16:54:46 +0200

    🔨Implement authentification

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,5 +1,6 @@
 import CustomButton from '@/components/CustomButton';
 import CustomInput from '@/components/CustomInput';
+import { signIn } from '@/lib/appwrite';
 import { Link, router } from "expo-router";
 import { useState } from "react";
 import { Alert, Text, View } from 'react-native';
@@ -9,12 +10,15 @@ const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
 
   const submit = async () => {
-    if(!form.email || !form.password) return Alert.alert("Error", "Please enter valid email address & password.");
+    const { email, password } = form;
+
+    if(!email || !password) return Alert.alert("Error", "Please enter valid email address & password.");
 
     setIsSubmitting(true);
 
     try {
-        Alert.alert("Sucess", "User signed in sucessfully.");
+        await signIn({ email, password });
+
         router.replace("/");
     } catch (error: any) {
         Alert.alert("Error", error.message);
